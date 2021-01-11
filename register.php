47ec987013ef1f5b719d22b2b5b2706d785100fd
dlanaras <dimitrislanaras04@outlook.com>
2021-01-11 22:06:02 +0100

    new webpage that takes user input

---
+++ register.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lernende registrieren</title>
+</head>
+<body>
+    <form action="private/writer.php" method="POST">
+        <input type="text" name="prename" placeholder="Vorname">
+        <br>
+        <input type="text" name="lastname" placeholder="Nachname">
+        <br>
+    </form>
+</body>
+</html>
